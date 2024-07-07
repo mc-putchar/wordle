badf6b46c38bef9a5e8be0908f46a9c80be93907
tesla33io <artemstavropolov@gmail.com>
2024-07-07 17:12:56 +0200

    feat: rework logic

--- backend/logic.py
+++ backend/logic.py
@@ -6,18 +6,51 @@
 #    By: astavrop <astavrop@student.42berlin.de>    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/07/07 15:27:44 by astavrop          #+#    #+#              #
-#    Updated: 2024/07/07 15:27:45 by astavrop         ###   ########.fr        #
+#    Updated: 2024/07/07 17:07:46 by astavrop         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 from collections import Counter
+import sys
+
+STATUS_CORRECT = "correct"
+STATUS_INCOMPLETE = "incomplete"
+STATUS_LOSER = "loser"
+STATUS_MISS = "missing"
+R_CORRECT = "correct"
+R_PRESENT = "present"
+R_ABSENT = "absent"
 
 
-def check_word(attempt: str):
-    letters = set(attempt)
-    counts = dict(Counter(list(attempt)))
-    print(counts)
+def check_word(attempt: str, secret: str):
+    counts = dict(Counter(secret))
+    result = []
+    print(f"Secret\t:\t{secret}")
+    print(f"User  \t:\t{attempt}")
+    print()
+    # LOOP 1
+    for i in range(len(attempt)):
+        if attempt[i] == secret[i]:
+            result.append(R_CORRECT)
+            counts[attempt[i]] -= 1
+        else:
+            result.append(R_ABSENT)
+    print(f"Result: {result}\nCounts: {counts}")
+    # LOOP 2
+    for i in range(len(attempt)):
+        if (attempt[i] in secret
+            and counts[attempt[i]] > 0
+            and result[i] == R_ABSENT):
+            result[i] = R_PRESENT
+            counts[attempt[i]] -= 1
+    print("After LOOP 2")
+    print(f"Result: {result}\nCounts: {counts}")
+    print(enumaret(result))
 
 
 if __name__ == "__main__":
-    check_word(input("word? "))
+    if len(sys.argv) > 1:
+        secret = sys.argv[1]
+    else:
+        secret = "hello"
+    check_word(input("word? "), secret)
